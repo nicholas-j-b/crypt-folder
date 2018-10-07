9799841b88c7c0f26213017922fcabeaa2333403
Nicholas Brooking <nicholas.j.brooking@gmail.com>
2018-10-07 14:52:23 +0200

    single file (hard) complement

--- src/crypt_folder.cc
+++ src/crypt_folder.cc
@@ -1,6 +1,21 @@
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <vector>
+
 
 int main(int argc, char** argv){
+  std::ifstream inFile("/home/nick/ws-cpp/crypt-folder/test/first.txt", std::ios::binary);
+  std::ofstream outFile("/home/nick/ws-cpp/crypt-folder/test/out.txt", std::ios::binary | std::ios::out);
+
+  std::vector<char> buffer((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+  if (outFile.is_open()){
+    for(auto item : buffer){
+      item = ~item;
+      outFile << item;
+    }
+    outFile.close();
+  }
 
   return 0;
 }
